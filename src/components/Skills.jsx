516c69bba56ab20fb69b10906b5d7a94430f832d
ITStanley <isharastanley2000@gmail.com>
2024-10-18 10:41:14 +0530

    Rearranged logos directory

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { motion } from "framer-motion";
+import htmlLogo from "../assets/logos/html.svg";
+import cssLogo from '../assets/logos/css.svg';
+import javascriptLogo from '../assets/logos/javascript.svg';
+import xmlLogo from '../assets/logos/xml.svg';
+import javaLogo from '../assets/logos/java.svg';
+import cLogo from '../assets/logos/c.svg';
+import pythonLogo from '../assets/logos/python.svg';
+// import sqlLogo from "../assets/logos/sql.svg";
+import bootstrapLogo from '../assets/logos/bootstrap.svg';
+import tailwindcssLogo from '../assets/logos/tailwindcss.svg';
+import reactLogo from '../assets/logos/react.svg';
 
 const Skills = () => {
   const upperMarquee = [
-    "/html.svg",
-    "/logos/css.svg",
-    "/logos/javascript.svg",
-    "/logos/xml.svg",
-    "/logos/java.svg",
-    "/logos/c.svg",
-    "/logos/python.svg",
-    "/logos/sql.svg",
-    "/logos/bootstrap.svg",
-    "/logos/tailwindcss.svg",
-    "/logos/react.svg",
+    htmlLogo,
+    cssLogo,
+    javascriptLogo,
+    xmlLogo,
+    javaLogo,
+    cLogo,
+    pythonLogo,
+    // sqlLogo,
+    bootstrapLogo,
+    tailwindcssLogo,
+    reactLogo,
   ];
 
   return (
     <div className="text-white max-w-[1200px] mx-auto mt-8 flex flex-col justify-start h-[50vh] py-6 text-center md:mt-30 md:py-0" id="about">
       
-      <h2 className="mb-10 text-4xl font-bold primary-color" data-aos="flip-left">Skills Hi</h2>
+      <h2 className="mb-10 text-4xl font-bold primary-color" data-aos="flip-left">Skills</h2>
 
       <div className="flex p-5 overflow-hidden border border-gray-600 md:h-[150px] max-w-[1200px] mx-auto md:flex md:justify-between md:items-center">
         <motion.div
