546acd5c5230808cd7346be71b8acf116bc3817a
ITStanley <isharastanley2000@gmail.com>
2024-10-27 17:53:03 +0530

    Simple Updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import github from "../assets/Icons/github.png";
+import linkedin from "../assets/Icons/linkedin.png";
+import whatsapp from "../assets/Icons/whatsapp.png";
+import mail from "../assets/Icons/gmail.png";
 
 const Footer = () => {
   return (
@@ -6,12 +10,76 @@ const Footer = () => {
     //   <span className="font-bold primary-color">@2024 Ishara Stanley</span>
     // </div>
 
-    <a className="flex relative items-center justify-center lg:w-[99.7%] h-[60px] p-[2px] bg-gradient-to-l from-purple-600 to-blue-600 ">
-        <div class="absolute -inset-1 bg-gradient-to-r from-purple-600 to-blue-600 blur opacity-100"></div>
-        <div class="relative w-full py-4 bg-[#01002c] text-center">
-        <span className="font-bold primary-color">@2024 Ishara Stanley</span>
+    // <div className="flex relative items-center justify-center w-[100%] p-[2px] bg-gradient-to-l from-purple-600 to-blue-600 ">
+    //     <div class="absolute -inset-1 bg-gradient-to-r from-purple-600 to-blue-600 blur opacity-100"></div>
+    //     <div class="relative w-full py-4 bg-[#01002c] text-center">
+    //     <span className="font-bold primary-color">@2024 Ishara Stanley</span>
+    //     </div>
+    // </div>
+    <div className="relative h-24 bg-blue-600">
+      <div className="absolute  inset-0 z-10 p-5 bg-[#01002c] mx-auto -mb-1 text-center">
+        <span className="mb-3 font-bold primary-color">@2024 Ishara Stanley</span>
+        
+        <div className="flex items-center justify-center w-full gap-3">
+          <a
+            className="flex items-center w-9 justify-center rounded-full p-[1.5px] bg-gradient-to-b to-purple-600 from-blue-600 "
+            href="https://github.com/ITStanley"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="relative group cursor-pointer">
+              <div class="absolute -inset-1 bg-gradient-to-t from-purple-600 to-blue-600 rounded-full blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div class="relative px-1 py-1 rounded-full bg-black">
+                <img src={github} alt="" className="h-6" />
+              </div>
+            </div>
+          </a>
+
+          <a
+            className="flex items-center w-9  justify-center rounded-full p-[1.5px] bg-gradient-to-b to-purple-600 from-blue-600 "
+            href="https://www.linkedin.com/in/ishara-stanley"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="relative group cursor-pointer">
+              <div class="absolute -inset-1 bg-gradient-to-t from-purple-600 to-blue-600 rounded-full blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div class="relative px-1 py-1 rounded-full bg-black">
+                <img src={linkedin} alt="" className="h-6" />
+              </div>
+            </div>
+          </a>
+
+          <a
+            className="flex items-center w-9  justify-center rounded-full p-[1.5px] bg-gradient-to-b to-purple-600 from-blue-600 "
+            href="https://wa.me/94770680475"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="relative group cursor-pointer">
+              <div class="absolute -inset-1 bg-gradient-to-t from-purple-600 to-blue-600 rounded-full blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div class="relative px-1 py-1 rounded-full bg-black">
+                <img src={whatsapp} alt="" className="h-6" />
+              </div>
+            </div>
+          </a>
+
+          <a
+            className="flex items-center w-9 justify-center rounded-full p-[1.5px] bg-gradient-to-b to-purple-600 from-blue-600 "
+            href="mailto: isharastanley2000@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="relative group cursor-pointer">
+              <div class="absolute -inset-1 bg-gradient-to-t from-purple-600 to-blue-600 rounded-full blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div class="relative px-1 py-1 rounded-full bg-black">
+                <img src={mail} alt="" className="h-6" />
+              </div>
+            </div>
+          </a>
         </div>
-    </a>
+      </div>
+      <div className="absolute z-0 w-full bg-gradient-to-r from-purple-600 to-blue-600 -inset-1 blur"></div>
+    </div>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -28,8 +28,7 @@ const Skills = () => {
       </h2>
 
       <div className="grid grid-cols-1 gap-32 p-3 lg:grid-cols-2">
-        <div>
-          
+        <div >
           <h3
             className="mb-8 text-2xl font-bold text-purple-600 underline"
             data-aos="fade-right"
@@ -123,15 +122,7 @@ const Skills = () => {
             Tools & Technologies
           </h3>
 
-          {/* <div className="flex flex-row">
-            <p className="text-left basis-1/2">HTML</p>
-            <p className="text-sm text-right text-gray-500 basis-1/2">80%</p>
-          </div>
-          <div className="h-2 mb-2 bg-gray-800 rounded-full">
-            <div className="w-[80%] h-2 bg-primary-color rounded-full"  data-aos="zoom-in-right" data-aos-delay="700"></div>
-          </div> */}
-
-          <div className="grid grid-cols-3 gap-5 mx-auto mt-20 sm:grid-cols-4" data-aos="zoom-in" data-aos-delay="700">
+          <div className="grid grid-cols-4 gap-5 mx-auto mt-20 sm:grid-cols-4" data-aos="zoom-in" data-aos-delay="700">
             <div className="h-[130px] p-4 bg-gray-800 rounded-md w-28 hover:bg-gray-700 hover:scale-110 hover:duration-500">
               <img src={vscode} alt="" className="" />
               <p className="my-2 text-xs">VS Code</p>
